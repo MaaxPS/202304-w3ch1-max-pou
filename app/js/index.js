028ecfa2a23c224925987cf593217ecc29a3eb5e
Max Pou Smith <maxpousmith10@gmail.com>
2023-04-18 08:37:17 +0200

    Fix characters abilities

--- app/js/index.js
+++ app/js/index.js
@@ -11,18 +11,18 @@ const createCard = (character) => {
   const body = document.querySelector("body");
 
   new DivComponent(body, "app container");
-  const appContainer = document.querySelector(".app.container");
+  const appContainer = body.querySelector(".app.container");
 
   new UlComponent(appContainer, "characters-list row list-unstyled");
-  const charactersList = document.querySelector(
+  const charactersList = appContainer.querySelector(
     ".characters-list.row.list-unstyled"
   );
 
   new LiComponent(charactersList, "character col");
-  const column = document.querySelector(".character.col");
+  const column = charactersList.querySelector(".character.col");
 
   new DivComponent(column, "card character__card");
-  const card = document.querySelector(".card.character__card");
+  const card = column.querySelector(".card.character__card");
 
   new ImgComponent(
     card,
@@ -31,7 +31,7 @@ const createCard = (character) => {
   );
 
   new DivComponent(card, "card-body");
-  const cardBody = document.querySelector(".card-body");
+  const cardBody = card.querySelector(".card-body");
 
   new IComponent(card, "emoji");
 
@@ -42,29 +42,43 @@ const createCard = (character) => {
   );
 
   new DivComponent(cardBody, "character__info");
-  const characterInfo = document.querySelector(".character__info");
+  const characterInfo = cardBody.querySelector(".character__info");
 
   new DivComponent(cardBody, "character__overlay");
-  const characterOverlay = document.querySelector(".character__overlay");
+  const characterOverlay = cardBody.querySelector(".character__overlay");
 
   new UlComponent(characterInfo, "list-unstyled");
-  const ageState = document.querySelector(".list-unstyled");
+  const ageState = characterInfo.querySelector(".list-unstyled");
 
   new LiComponent(ageState, "", "Age: " + character.characterData.age + " yrs");
 
   new LiComponent(ageState, "stateLike", "State: ");
-  const state = document.querySelector(".stateLike");
+  const state = ageState.querySelector(".stateLike");
 
   new IComponent(state, "fas fa-thumbs-down");
   new IComponent(state, "fas fa-thumbs-up");
 
   new UlComponent(characterOverlay, "list-unstyled");
-  const unstyled = document.querySelector(".list-unstyled");
+  const unstyled = characterOverlay.querySelector(".list-unstyled");
 
   new DivComponent(characterOverlay, "character__actions");
 
   if ("advises" in character) {
     new LiComponent(unstyled, "", "Advises to: " + character.advises);
+  }
+
+  if ("dexterity" in character && "weapon" in character) {
+    new LiComponent(unstyled, "", "Weapon: " + character.weapon);
+    new LiComponent(unstyled, "", "Dexterity: " + character.dexterity);
+  }
+
+  if ("yearsOfReign" in character) {
+    new LiComponent(unstyled, "", "Years of Reign: " + character.yearsOfReign);
+  }
+
+  if ("kissAssLeve" in character && "serves" in character) {
+    new LiComponent(unstyled, "", "Kiss Ass Level: " + character.kissAssLeve);
+    new LiComponent(unstyled, "", "Serves to: " + character.serves);
   }
 };
 
